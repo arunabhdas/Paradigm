8d4d636a573b124923c6ccfd35602f9d1a4a17cb
Arunabh Das <arunabhdas@gmail.com>
2025-02-02 04:22:13 -0500

    Improved splash screen logic to load fonts

--- paradigmatic-app-react-native/Paradigmatic/app/_layout.js
+++ paradigmatic-app-react-native/Paradigmatic/app/_layout.js
@@ -1,7 +1,50 @@
 import { Stack } from "expo-router";
+import { useCallback, useEffect } from "react";
+import { useFonts } from "expo-font";
+import { View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        const hideSplash = async () => {
+            try {
+                if (fontsLoaded) {
+                    // Hide the splash screen after the fonts have loaded and the
+                    // UI is ready.
+                    await SplashScreen.hideAsync();
+                }
+            } catch (e) {
+                console.warn('Error hiding splash screen:', e);
+            }
+        };
+
+        hideSplash();
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <Stack 
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#FFFFFF",
+                },
+                headerShadowVisible: false,
+                headerTitle: "",
+            }}
+        />
+    );
 };
 
 export default Layout;
--- paradigmatic-app-react-native/Paradigmatic/components/home/welcome/Welcome.jsx
+++ paradigmatic-app-react-native/Paradigmatic/components/home/welcome/Welcome.jsx
@@ -1,13 +1,39 @@
+import { useState } from 'react'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+
+import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Welcome</Text>
+  <View>
+    <View style={styles.container}>
+      <Text style={styles.userName}>Paradigmatic</Text>
+      <Text style={styles.welcomeMessage}>Find alternative supplier and markets</Text>
     </View>
+
+    <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        <TextInput
+          style={styles.searchInput}
+          value=""
+          onChange={() => {}}
+          placeholder="What are you looking for?"
+        />
+      </View>
+    </View>
+  </View>
   )
 }
 
