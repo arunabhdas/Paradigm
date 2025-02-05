fc2e871bcd0fea0a7c3deec3c8d7dd85a28ec890
Arunabh Das <arunabhdas@gmail.com>
2025-02-05 01:33:38 -0500

    Displaying list of nearby

--- paradigmatic-app-react-native/Paradigmatic/components/common/cards/popular/PopularProductCard.jsx
+++ paradigmatic-app-react-native/Paradigmatic/components/common/cards/popular/PopularProductCard.jsx
@@ -21,7 +21,7 @@ const PopularProductCard = ({ item, selectedJob, handleCardPress }) => {
       <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
       <View style={styles.infoContainer}>
         <Text style={styles.jobName} numberOfLines={1}>
-          {item.job_title}
+          {item.jobName}
         </Text>
         <Text style={styles.location}>{item.job_country}</Text>
       </View>
--- paradigmatic-app-react-native/Paradigmatic/components/home/nearby/Nearbytasks.jsx
+++ paradigmatic-app-react-native/Paradigmatic/components/home/nearby/Nearbytasks.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { React } from 'react'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+
 
 import styles from './nearbytasks.style'
 
+import { COLORS, SIZES } from '../../../constants';
+import PopularProductCard from '../../common/cards/nearby/NearbyTaskCard';
+import useFetch from '../../../hook/useFetch';
+
+const supplierTypes = ["Onions", "Tomato", "Potatoes", "Chilli Peppers", "Green Peppers"]
+
 const Nearbytasks = () => {
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch(
+    'search', { 
+      query: 'iOS developer jobs Remote',
+      num_pages: '1',
+    }
+  )
+
+
+  console.log(data);
+  
   return (
-    <View>
-      <Text>Shop Hyper Local</Text>
+    <View styles={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Shop Nearby</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((item) => (
+            <PopularProductCard 
+            key={item?.job_id}
+            item={item}
+            />
+          ))
+        )} 
+      </View>
+
     </View>
   )
 }
