519cbc7bf760c750fded56a4fe625b19235b9ccf
Arunabh Das <arunabhdas@gmail.com>
2025-02-05 00:24:48 -0500

    Displaying location and country

--- paradigmatic-app-react-native/Paradigmatic/components/common/cards/popular/PopularProductCard.jsx
+++ paradigmatic-app-react-native/Paradigmatic/components/common/cards/popular/PopularProductCard.jsx
@@ -17,6 +17,12 @@ const PopularProductCard = ({ item, selectedJob, handleCardPress }) => {
         />
       </TouchableOpacity>
       <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
