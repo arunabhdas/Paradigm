3594ef667bd255bbac430f1e8b38b506e3e05ee7
Arunabh Das <arunabhdas@gmail.com>
2025-02-05 02:24:12 -0500

    Implemented task-detail screen

--- paradigmatic-app-react-native/Paradigmatic/app/task-details/[id].js
+++ paradigmatic-app-react-native/Paradigmatic/app/task-details/[id].js
@@ -1,10 +1,40 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+    View, 
+    Text, 
+    SafeAreaView, 
+    ScrollView, 
+    ActivityIndicator, 
+    RefreshControl 
+} from 'react-native';
+import { Stack, useRouter, useLocalSearchParams } from 'expo-router';
+
+import { useCallback, useState } from 'react';
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+
+import { COLORS, icons, SIZES } from '../../constants';
+
+import useFetch from '../../hook/useFetch';
+
+
 
 const TaskDetails = () => {
-  return (
-    <Text>TaskDetails</Text>
-  )
+    const params = useLocalSearchParams();
+
+    const router = useRouter();
+
+    const { data, isLoading, error, refetch } = useFetch(
+        'job-details', {
+            job_id: params.id
+        }
+    );
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightGreen }}>
+        <Stack.Screen></Stack.Screen>
+        </SafeAreaView>
+    )
 }
 
 export default TaskDetails
