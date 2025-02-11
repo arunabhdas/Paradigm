4563677b63cc86ad5bca6e7f63978fc75d023523
Arunabh Das <arunabhdas@gmail.com>
2025-02-11 05:20:51 -0500

    Fixed version conflicts

--- paradigmatic-app-compose-multiplatform/ParadigmaticApp/composeApp/build.gradle.kts
+++ paradigmatic-app-compose-multiplatform/ParadigmaticApp/composeApp/build.gradle.kts
@@ -31,28 +31,7 @@ kotlin {
             isStatic = true
         }
     }
-    
-    jvm("desktop")
-    
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        moduleName = "composeApp"
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                outputFileName = "composeApp.js"
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-        binaries.executable()
-    }
+
     
     sourceSets {
         androidMain.dependencies {
