48b11bd098425e4a2c71f13c7c572b68bed4a1a6
Arunabh Das <arunabhdas@gmail.com>
2025-02-12 05:00:31 -0500

    Added lightScheme and darkScheme

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/build.gradle.kts
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/build.gradle.kts
@@ -98,6 +98,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.material3.android)
     debugImplementation(compose.uiTooling)
 }
 
--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/App.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/App.kt
@@ -2,6 +2,7 @@ package app.paradigmatic.paradigmaticapp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -10,28 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import app.paradigmatic.paradigmaticapp.ui.theme.darkScheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import paradigmatic.composeapp.generated.resources.Res
 import paradigmatic.composeapp.generated.resources.compose_multiplatform
 
+import app.paradigmatic.paradigmaticapp.ui.theme.lightScheme
+import app.paradigmatic.paradigmaticapp.ui.theme.darkScheme
+
+
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+    val colors = if (!isSystemInDarkTheme()) {
+        lightScheme
+    } else {
+        darkScheme
+    }
+    MaterialTheme(colorScheme = colors) {
+
     }
 }
