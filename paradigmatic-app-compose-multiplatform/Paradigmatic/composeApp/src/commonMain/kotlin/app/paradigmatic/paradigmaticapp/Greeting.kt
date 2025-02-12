fc600952d59014d8e9e34e732549dd571e85d53f
Arunabh Das <arunabhdas@gmail.com>
2025-02-12 03:27:33 -0500

    Created Paradigmatic using kmp.jetbrains.com Kotlin Multiplatform Wizard

---
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/Greeting.kt
@@ -0,0 +1,9 @@
+package app.paradigmatic.paradigmaticapp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
