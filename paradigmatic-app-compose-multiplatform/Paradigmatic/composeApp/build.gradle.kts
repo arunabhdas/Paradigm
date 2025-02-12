689edd5bc3d8824ec1c4c823d60cc0bcff962a35
Arunabh Das <arunabhdas@gmail.com>
2025-02-12 04:03:59 -0500

    Added kotlinxSerialization and realm plugin to composeApp and project build.gradle.kts

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/build.gradle.kts
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.realm.plugin)
 }
 
 kotlin {
