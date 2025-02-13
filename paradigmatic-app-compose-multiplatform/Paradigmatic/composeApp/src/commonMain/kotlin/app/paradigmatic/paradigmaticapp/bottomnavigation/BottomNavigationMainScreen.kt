a0353e58204d1efb4045c7f3fe27dea9b527870d
Arunabh Das <arunabhdas@gmail.com>
2025-02-13 17:22:22 -0500

    Ran optimize imports

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/BottomNavigationMainScreen.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/BottomNavigationMainScreen.kt
@@ -3,11 +3,23 @@ package app.paradigmatic.paradigmaticapp.bottomnavigation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import app.paradigmatic.paradigmaticapp.navigation.*
 import cafe.adriel.voyager.core.screen.Screen
 
 class BottomNavigationMainScreen : Screen {
