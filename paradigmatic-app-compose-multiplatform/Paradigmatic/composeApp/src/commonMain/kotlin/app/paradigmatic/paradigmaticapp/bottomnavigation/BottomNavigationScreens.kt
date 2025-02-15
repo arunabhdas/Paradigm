b7d6bfebf0640c519d0513e6dfaa07f1c0a1eba6
Arunabh Das <arunabhdas@gmail.com>
2025-02-15 06:12:54 -0500

    Added CurrencyView composable

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/BottomNavigationScreens.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/BottomNavigationScreens.kt
@@ -1,31 +1,47 @@
 package app.paradigmatic.paradigmaticapp.bottomnavigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import app.paradigmatic.paradigmaticapp.data.remote.api.CurrencyApiServiceImpl
 import app.paradigmatic.paradigmaticapp.domain.CurrencyApiService
 import app.paradigmatic.paradigmaticapp.domain.PreferencesRepository
+import app.paradigmatic.paradigmaticapp.domain.model.Currency
+import app.paradigmatic.paradigmaticapp.domain.model.CurrencyCode
 import app.paradigmatic.paradigmaticapp.domain.model.RateStatus
+import app.paradigmatic.paradigmaticapp.domain.model.RequestState
 import app.paradigmatic.paradigmaticapp.presentation.component.HomeHeader
 import app.paradigmatic.paradigmaticapp.presentation.screen.HomeUiEvent
 import app.paradigmatic.paradigmaticapp.presentation.screen.HomeViewModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
+import org.jetbrains.compose.resources.painterResource
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -153,3 +169,51 @@ fun TabFiveScreen() {
         }
     }
 }
+
+@Composable
+fun RowScope.CurrencyView(
+    placeholder: String,
+    currency: RequestState<Currency>,
+    onClick: () -> Unit
+) {
+    Column(modifier = Modifier.weight(1f)) {
+        Text(
+            modifier = Modifier.padding(start = 12.dp),
+            text = placeholder,
+            fontSize = MaterialTheme.typography.bodySmall.fontSize,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(size = 8.dp))
+                .background(Color.White.copy(alpha = 0.05f))
+                .height(54.dp)
+                .clickable { onClick() },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            if (currency.isSuccess()) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(
+                        CurrencyCode.valueOf(
+                            currency.getSuccessData().code
+                        ).flag
+                    ),
+                    tint = Color.Unspecified,
+                    contentDescription = "Country Flag"
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = CurrencyCode.valueOf(currency.getSuccessData().code).name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
