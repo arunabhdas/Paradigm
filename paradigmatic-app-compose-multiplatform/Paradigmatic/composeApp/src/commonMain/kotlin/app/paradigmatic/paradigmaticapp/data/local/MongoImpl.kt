527bf650d4dfdea0add7861920bfcc4a2263b6b8
Arunabh Das <arunabhdas@gmail.com>
2025-02-16 05:37:18 -0500

    Implementation of configureTheRealm

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/data/local/MongoImpl.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/data/local/MongoImpl.kt
@@ -4,13 +4,21 @@ import app.paradigmatic.paradigmaticapp.domain.MongoRepository
 import app.paradigmatic.paradigmaticapp.domain.model.Currency
 import app.paradigmatic.paradigmaticapp.domain.model.CurrencyApiRequestState
 import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
 import kotlinx.coroutines.flow.Flow
 
 class MongoImpl: MongoRepository {
     private var realm: Realm? = null
 
     override fun configureTheRealm() {
-        TODO("Not yet implemented")
+        if (realm == null || realm!!.isClosed()) {
+           val config = RealmConfiguration.Builder(
+               schema = setOf(Currency::class)
+           )
+               .compactOnLaunch()
+               .build()
+           realm = Realm.open(config)
+        }
     }
 
     override suspend fun insertCurrencyData(currency: Currency) {
