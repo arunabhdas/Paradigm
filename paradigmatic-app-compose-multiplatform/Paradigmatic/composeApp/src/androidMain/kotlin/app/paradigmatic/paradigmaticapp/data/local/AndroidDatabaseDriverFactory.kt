74663ea3812be9fad685c391cedfbaa45941c4c3
Arunabh Das <arunabhdas@gmail.com>
2025-02-24 03:18:25 -0500

    Created AndroidDatabaseDriverFactory implementation of DatabaseDriverFactory interface

---
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/androidMain/kotlin/app/paradigmatic/paradigmaticapp/data/local/AndroidDatabaseDriverFactory.kt
@@ -0,0 +1,18 @@
+package app.paradigmatic.paradigmaticapp.data.local
+
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import app.paradigmatic.paradigmaticapp.ParadigmaticDatabase
+
+class AndroidDatabaseDriverFactory (
+    private val context: Context
+): DatabaseDriverFactory {
+    override fun createDriver(): SqlDriver {
+        return AndroidSqliteDriver(
+            ParadigmaticDatabase.Schema,
+            context,
+            "paradigmatic.db"
+        )
+    }
+}
