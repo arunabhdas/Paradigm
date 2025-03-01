7b13d6a9024d79716f35e0f72e3533b13bc33f57
Arunabh Das <arunabhdas@gmail.com>
2025-03-01 03:36:57 -0500

    Added MemeViewModel

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
@@ -18,12 +18,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import app.paradigmatic.paradigmaticapp.presentation.viewmodel.MemeViewModel
 import app.paradigmatic.paradigmaticapp.ui.theme.surfaceContainerDark
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.component.KoinComponent
 
 
@@ -36,6 +39,11 @@ class TabFourScreen(
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
+        val viewModel = koinViewModel<MemeViewModel>()
+        val memes by viewModel.memes
+        val sortedByFavorite = viewModel.sortedByFavorite
+
+
         Scaffold (
             floatingActionButton = {
                 FloatingActionButton(onClick = {}){
--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/presentation/viewmodel/MemeViewModel.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/presentation/viewmodel/MemeViewModel.kt
@@ -1,13 +1,18 @@
 package app.paradigmatic.paradigmaticapp.presentation.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import app.paradigmatic.paradigmaticapp.data.room.MemeDatabase
+import app.paradigmatic.paradigmaticapp.domain.model.Meme
+import app.paradigmatic.paradigmaticapp.domain.model.RequestState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.mutableStateOf
 
 class MemeViewModel (
     private val database: MemeDatabase
@@ -16,11 +21,20 @@ class MemeViewModel (
     private var _sortedByFavorite = MutableStateFlow(false)
     val sortedByFavorite: StateFlow<Boolean> = _sortedByFavorite
 
+    private var _memes: MutableState<RequestState<List<Meme>>> = mutableStateOf(RequestState.Loading)
+    val memes: State<RequestState<List<Meme>>> = _memes
+
     init {
         viewModelScope.launch {
             database.memeDao().readAllMemes().collectLatest {
-
+                _memes.value = RequestState.Success(
+                    data = it
+                )
             }
         }
+    }
+
+    fun toggleSortByFavorite() {
+        _sortedByFavorite.value = !_sortedByFavorite.value
     }
 }
