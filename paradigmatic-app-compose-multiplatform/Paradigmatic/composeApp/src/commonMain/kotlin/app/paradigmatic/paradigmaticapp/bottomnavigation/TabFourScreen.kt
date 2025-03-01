e0734b7f629c5fd9e325160b744c435ec79be484
Arunabh Das <arunabhdas@gmail.com>
2025-03-01 02:11:45 -0500

    Added View Memes button to TabFourScreen

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
@@ -1,5 +1,6 @@
 package app.paradigmatic.paradigmaticapp.bottomnavigation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +10,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import app.paradigmatic.paradigmaticapp.ui.theme.surfaceContainerDark
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import org.koin.core.component.KoinComponent
 
 
@@ -23,6 +27,7 @@ import org.koin.core.component.KoinComponent
 class TabFourScreen: Screen, KoinComponent {
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.current
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
@@ -37,7 +42,15 @@ class TabFourScreen: Screen, KoinComponent {
                     modifier = Modifier.size(48.dp)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                Text("Alerts Screen")
+                Button(
+                    onClick = {
+                        navigator?.push(TabFourScreenDetail(0))
+                    },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                        .background(surfaceContainerDark)
+                ) {
+                    Text(text = "View Memes")
+                }
             }
         }
     }
