0857302e9ead80a7feb5ce8d5651907e31605736
Arunabh Das <arunabhdas@gmail.com>
2025-03-01 02:00:17 -0500

    Added TabFourScreen and TabFourScreenDetail

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreenDetail.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreenDetail.kt
@@ -23,7 +23,7 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 
-data class TabTwoScreenDetail(val number: Int) : Screen {
+data class TabFourScreenDetail(val number: Int) : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
