ef8298f987734ea0d16aec290c59d5529f5f2c6c
Arunabh Das <arunabhdas@gmail.com>
2025-03-01 02:27:13 -0500

    Added FloatingActionButton to TabFourScreen

--- paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
+++ paradigmatic-app-compose-multiplatform/Paradigmatic/composeApp/src/commonMain/kotlin/app/paradigmatic/paradigmaticapp/bottomnavigation/TabFourScreen.kt
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,35 +28,48 @@ import org.koin.core.component.KoinComponent
 
 
 
-class TabFourScreen: Screen, KoinComponent {
+class TabFourScreen(
+    onMemeSelect: (Int) -> Unit,
+    onCreateClick: () -> Unit
+): Screen, KoinComponent {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+        Scaffold (
+            floatingActionButton = {
+                FloatingActionButton(onClick = {}){
+
+                }
+            }
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "Bookmarks",
-                    modifier = Modifier.size(48.dp)
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = {
-                        navigator?.push(TabFourScreenDetail(0))
-                    },
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                        .background(surfaceContainerDark)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Text(text = "View Memes")
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Bookmarks",
+                        modifier = Modifier.size(48.dp)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            navigator?.push(TabFourScreenDetail(0))
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                            .background(surfaceContainerDark)
+                    ) {
+                        Text(text = "View Memes")
+                    }
                 }
             }
         }
+
     }
 
 }
