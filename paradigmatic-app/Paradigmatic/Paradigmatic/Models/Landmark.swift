9a4a8fcbe7cd6ee2c30ab740a4686fe366bd2259
Arunabh Das <arunabhdas@gmail.com>
2025-07-31 13:24:32 -0400

    Added Landmark.swift data model

---
+++ paradigmatic-app/Paradigmatic/Paradigmatic/Models/Landmark.swift
@@ -0,0 +1,15 @@
+//
+//  Landmark.swift
+//  Paradigmatic
+//
+//  Created by Coder on 7/31/25.
+//
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+}
