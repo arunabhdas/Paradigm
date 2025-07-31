fac4b03aa225a40bea3e0bc14feca986967f525d
Arunabh Das <arunabhdas@gmail.com>
2025-07-31 14:58:15 -0400

    Added locationCoordinate2D computed property for coordinates

--- paradigmatic-app/Paradigmatic/Paradigmatic/Models/Landmark.swift
+++ paradigmatic-app/Paradigmatic/Paradigmatic/Models/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import MapKit
 
 
 struct Landmark: Hashable, Codable {
@@ -20,6 +21,21 @@ struct Landmark: Hashable, Codable {
     
     var image: Image {
         Image(self.imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    var locationCoordinate2D: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+        
     }
 }
 
